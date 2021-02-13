05f113229c1c4b60723f36abbd496a5ccdb89b45
Peony Lum <peonylum@MacBook-Pro-2.local>
2021-02-13 06:02:58 -0800

    Complete Steps 1--5

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,27 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  const prod_list = document.getElementById('product-list');
+  var prod_JSON = '';
+
+  if (localStorage.getItem('JSONObj') == null) {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => localStorage.setItem('JSONObj', JSON.stringify(data)))
+      .then(() => {
+        prod_JSON = eval(localStorage.getItem('JSONObj'));
+
+        prod_JSON.forEach(o => {
+          prod_list.innerHTML += `<product-item image ='${o.image}' title='${o.title}' price='${o.price}'/>`
+        }) 
+      }); 
+  }
+  else {
+    prod_JSON = eval(localStorage.getItem('JSONObj'));
+
+    prod_JSON.forEach(o => {
+      prod_list.innerHTML += `<product-item image ='${o.image}' title='${o.title}' price='${o.price}'/>`
+    })
+
+  }
 });
